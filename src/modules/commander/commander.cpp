PX4/PX4-Autopilot

goto RTL if offboard is lost for a specified time

src/modules/commander/commander.cpp
@@ -1090,6 +1090,7 @@ int commander_thread_main(int argc, char *argv[])
 	param_t _param_geofence_action = param_find("GF_ACTION");
 	param_t _param_disarm_land = param_find("COM_DISARM_LAND");
 	param_t _param_map_mode_sw = param_find("RC_MAP_MODE_SW");
+	param_t _param_of_rtl = param_find("COM_OF_RTL");
 
 	// These are too verbose, but we will retain them a little longer
 	// until we are sure we really don't need them.
@@ -1177,6 +1178,7 @@ int commander_thread_main(int argc, char *argv[])
 	status.rc_signal_lost = true;
 	status.offboard_control_signal_lost = true;
 	status.data_link_lost = true;
+	status.offboard_control_lost_rtl_timestamp = 0;
 
 	/* set battery warning flag */
 	status.battery_warning = vehicle_status_s::VEHICLE_BATTERY_WARNING_NONE;
@@ -1436,6 +1438,7 @@ int commander_thread_main(int argc, char *argv[])
 	int32_t datalink_loss_timeout = 10;
 	float rc_loss_timeout = 0.5;
 	int32_t datalink_regain_timeout = 0;
+	float offboard_rtl_timeout = -1.0f;
 
 	int32_t geofence_action = 0;
 
@@ -1542,6 +1545,7 @@ int commander_thread_main(int argc, char *argv[])
 			param_get(_param_geofence_action, &geofence_action);
 			param_get(_param_disarm_land, &disarm_when_landed);
 			param_get(_param_map_mode_sw, &map_mode_sw);
+			param_get(_param_of_rtl, &offboard_rtl_timeout);
 
 			/* Autostart id */
 			param_get(_param_autostart_id, &autostart_id);
@@ -1582,6 +1586,13 @@ int commander_thread_main(int argc, char *argv[])
 		} else {
 			if (!status.offboard_control_signal_lost) {
 				status.offboard_control_signal_lost = true;
+				if (offboard_rtl_timeout < 0) {
+					status.offboard_control_lost_rtl_timestamp = 0;
+
+				} else {
+					status.offboard_control_lost_rtl_timestamp = hrt_absolute_time() +
+						(float)1e6 * offboard_rtl_timeout;
+				}
 				status_changed = true;
 			}
 		}

src/modules/commander/state_machine_helper.cpp
@@ -814,7 +814,18 @@ bool set_nav_state(struct vehicle_status_s *status, const bool data_link_loss_en
 			status->failsafe = true;
 
 			if (status->condition_local_position_valid) {
-				status->nav_state = vehicle_status_s::NAVIGATION_STATE_LAND;
+				if (status->offboard_control_lost_rtl_timestamp == 0) {
+					/* RTL when offboard lost is disabled */
+					status->nav_state = vehicle_status_s::NAVIGATION_STATE_LAND;
+
+				} else if (status->offboard_control_lost_rtl_timestamp > hrt_absolute_time()) {
+					/* wait before switching to RTL */
+					status->nav_state = vehicle_status_s::NAVIGATION_STATE_AUTO_LOITER;
+
+				} else {
+					status->nav_state = vehicle_status_s::NAVIGATION_STATE_AUTO_RTL;
+				}
+
 			} else if (status->condition_local_altitude_valid) {
 				status->nav_state = vehicle_status_s::NAVIGATION_STATE_DESCEND;
 			} else {

volutail/Firmware

Conflicts:
	msg/vehicle_status.msg
	src/modules/commander/commander.cpp
	src/modules/commander/commander_params.c
	src/modules/commander/state_machine_helper.cpp

src/modules/commander/commander.cpp
@@ -1188,6 +1188,7 @@ int commander_thread_main(int argc, char *argv[])
 	param_t _param_geofence_action = param_find("GF_ACTION");
 	param_t _param_disarm_land = param_find("COM_DISARM_LAND");
 	param_t _param_low_bat_act = param_find("COM_LOW_BAT_ACT");
+	param_t _param_of_rtl = param_find("COM_OF_RTL");
 
 	param_t _param_fmode_1 = param_find("COM_FLTMODE1");
 	param_t _param_fmode_2 = param_find("COM_FLTMODE2");
@@ -1277,6 +1278,7 @@ int commander_thread_main(int argc, char *argv[])
 	status.rc_signal_lost = true;
 	status_flags.offboard_control_signal_lost = true;
 	status.data_link_lost = true;
+	status.offboard_control_lost_rtl_timestamp = 0;
 
 	status_flags.condition_system_prearm_error_reported = false;
 	status_flags.condition_system_hotplug_timeout = false;
@@ -1531,6 +1533,7 @@ int commander_thread_main(int argc, char *argv[])
 	int32_t datalink_loss_timeout = 10;
 	float rc_loss_timeout = 0.5;
 	int32_t datalink_regain_timeout = 0;
+	float offboard_rtl_timeout = -1.0f;
 
 	int32_t geofence_action = 0;
 
@@ -1623,6 +1626,7 @@ int commander_thread_main(int argc, char *argv[])
 			param_get(_param_geofence_action, &geofence_action);
 			param_get(_param_disarm_land, &disarm_when_landed);
 			param_get(_param_low_bat_act, &low_bat_action);
+			param_get(_param_of_rtl, &offboard_rtl_timeout);
 
 			/* Autostart id */
 			param_get(_param_autostart_id, &autostart_id);
@@ -1671,6 +1675,13 @@ int commander_thread_main(int argc, char *argv[])
 		} else {
 			if (!status_flags.offboard_control_signal_lost) {
 				status_flags.offboard_control_signal_lost = true;
+				if (offboard_rtl_timeout < 0) {
+					status_flags.offboard_control_lost_rtl_timestamp = 0;
+
+				} else {
+					status_flags.offboard_control_lost_rtl_timestamp = hrt_absolute_time() +
+						(float)1e6 * offboard_rtl_timeout;
+				}
 				status_changed = true;
 			}
 		}

src/modules/commander/state_machine_helper.cpp
@@ -851,8 +851,19 @@ bool set_nav_state(struct vehicle_status_s *status, struct commander_state_s *in
 			status->failsafe = true;
 
 			if (status_flags->condition_local_position_valid) {
-				status->nav_state = vehicle_status_s::NAVIGATION_STATE_AUTO_LAND;
-			} else if (status_flags->condition_local_altitude_valid) {
+				if (status_flags->offboard_control_lost_rtl_timestamp == 0) {
+					/* RTL when offboard lost is disabled */
+					status->nav_state = vehicle_status_s::NAVIGATION_STATE_LAND;
+
+				} else if (status_flags->offboard_control_lost_rtl_timestamp > hrt_absolute_time()) {
+					/* wait before switching to RTL */
+					status->nav_state = vehicle_status_s::NAVIGATION_STATE_AUTO_LOITER;
+
+				} else {
+					status->nav_state = vehicle_status_s::NAVIGATION_STATE_AUTO_RTL;
+				}
+
+			} else if (status->condition_local_altitude_valid) {
 				status->nav_state = vehicle_status_s::NAVIGATION_STATE_DESCEND;
 			} else {
 				status->nav_state = vehicle_status_s::NAVIGATION_STATE_TERMINATION;